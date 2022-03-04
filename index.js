0b379b30cfb4fb1ce13592a54d93887cfbda4e73
alexrod94 <alexrod94@gmail.com>
2022-03-04 11:23:01 +0100

    Creado html

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import fetch from "node-fetch";
 
 async function getWithFetch() {
-  const res = await fetch(
-    "https://622108eeafd560ea69a58cbf.mockapi.io/Users/1"
-  );
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const finalRes = await res.json();
   console.log(finalRes);
 }
@@ -19,16 +17,40 @@ async function postWithFetch() {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
-      "Content-type": "application/json",
+      "Content-Type": "application/json",
     },
   });
   const finalRes = await res.json();
   console.log(finalRes);
 }
 
-async function putWithFetch() {}
+async function putWithFetch() {
+  const data = {
+    title: "Mi dato",
+    body: "Este es mi dato",
+    userId: 1,
+  };
 
-async function deleteWithFetch() {}
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/12", {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const finalRes = await res.json();
+  console.log(finalRes);
+}
+
+async function deleteWithFetch() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/12", {
+    method: "DELETE",
+  });
+  const finalRes = await res.json();
+  console.log(finalRes);
+}
 
 // getWithFetch();
-postWithFetch();
+// putWithFetch();
+
+deleteWithFetch();
